67a613459d8515d2988f9932289df6190abd912c
Ben Getsug <ben@s2nventures.com>
2022-01-07 21:23:06 -0600

    adding IconWidgetStepper

--- lib/stepper.dart
+++ lib/stepper.dart
@@ -1,6 +1,7 @@
 library stepper;
 
+export 'src/dot_stepper/dot_stepper.dart';
 export 'src/first_stepper/icon_stepper.dart';
+export 'src/first_stepper/icon_widget_stepper.dart';
 export 'src/first_stepper/image_stepper.dart';
 export 'src/first_stepper/number_stepper.dart';
-export 'src/dot_stepper/dot_stepper.dart';
